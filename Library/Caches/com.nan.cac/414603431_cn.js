0e3e53f367b2701deb89d43b318984845e9be64d
GitHub Action <action@github.com>
2024-11-27 10:52:45 +0000

    save caches

--- Library/Caches/com.nan.cac/414603431_cn.js
+++ Library/Caches/com.nan.cac/414603431_cn.js
@@ -3,7 +3,7 @@
   "bundle": "com.tencent.QQMusic",
   "id": 414603431,
   "name": "QQ音乐 - 听我想听",
-  "version": "13.11.1",
+  "version": "13.12.0",
   "price": "免费",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/f4/56/9d/f4569d49-bedd-d110-4b19-336af1b79a52/AppIcon-0-1x_U007ephone-0-0-0-1-0-0-sRGB-85-220-0.png/100x100bb.jpg"
 }
