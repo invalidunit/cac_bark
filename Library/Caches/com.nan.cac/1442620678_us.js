ab8e5d47517ecb75e0b3804c6244c4b2051fbcd2
GitHub Action <action@github.com>
2025-02-20 09:31:17 +0000

    save caches

--- Library/Caches/com.nan.cac/1442620678_us.js
+++ Library/Caches/com.nan.cac/1442620678_us.js
@@ -3,7 +3,7 @@
   "bundle": "com.nssurge.inc.surge-ios",
   "id": 1442620678,
   "name": "Surge 5",
-  "version": "5.14.3",
+  "version": "5.14.4",
   "price": "Free",
   "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/87/f9/11/87f91160-6158-1d0d-933a-bb2b8b22228c/AppIconS-0-0-1x_U007epad-0-1-0-0-85-220.png/100x100bb.jpg"
 }
